8c79e2b0d378eba8bc0d2c3b3e22fbdc20e7a5e3
liamyi <mrli.y@foxmail.com>
2024-12-15 15:21:03 +0800

    lsp ok

--- lua/config/plugins/cmp.lua
+++ lua/config/plugins/cmp.lua
@@ -2,12 +2,50 @@ return {
     {
         "hrsh7th/nvim-cmp",
         config = function()
-            require("cmp").setup {
+            local cmp = require("cmp")
+            local luasnip = require("cmp")
+            cmp.setup {
                 snippet = {
                     expand = function(args)
                         require 'luasnip'.lsp_expand(args.body)
                     end
                 },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-e>'] = cmp.mapping.abort(), -- 取消补全，esc也可以退出
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expandable() then
+                            luasnip.expand()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        elseif check_backspace() then
+                            fallback()
+                        else
+                            fallback()
+                        end
+                    end, {
+                        "i",
+                        "s",
+
+                    }),
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, {
+                        "i",
+                        "s",
+                    }),
+                }),
 
                 sources = {
                     { name = 'nvim_lsp' },
